2158f8b06e84d89208692ca288c81898fcdfc15e
d3ffy <tv4570@gmail.com>
2023-11-20 16:12:18 +0700

    Initial Commit

--- SlideNumberPuzzle/ContentView.swift
+++ SlideNumberPuzzle/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let columns = Array(repeating: GridItem(), count: 4)
+    @State var message: String = "Hello"
+    @State var tapCountMsg: String = "Moved: 0"
+    @State var tapCount: Int = 0
+    @State var numbers = Array(0..<16)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(numbers, id: \.self) {
+                    number in NumberGridCell(number: number)
+                        .onTapGesture {
+                            tapCount += 1
+                            message = "Tapped: \(number)"
+                            tapCountMsg = "Moved: \(tapCount)"
+                        }
+                }
+            }
+            .padding()
+            .onAppear{shuffle()}
+            
+            Text(message)
+            Text(tapCountMsg)
         }
-        .padding()
+    }
+    
+    func shuffle() {
+        numbers.shuffle()
+    }
+}
+
+struct NumberGridCell: View {
+    let BUTTON_WIDTH: CGFloat = 80
+    let BUTTON_HEIGHT: CGFloat = 80
+    let number: Int
+
+    var body: some View {
+        Text(number == 0 ? "" : "\(number)")
+            .frame(width: BUTTON_WIDTH, height: BUTTON_HEIGHT)
+            .background(number == 0 ? Color.clear : Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
     }
 }
 
